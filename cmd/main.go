492c0876666ec0f981f02dfec51d3429667dd7cc
Masahiro Yokoyama <part7steelballrun.to.8@gmail.com>
2025-02-12 22:32:46 +0900

    dig の trial (#2)
    * docs: 📝 dig のインストールとドキュメントを追加する
    
    * feat: ✨ dig の簡単なサンプルを追加する

--- cmd/main.go
+++ cmd/main.go
@@ -5,19 +5,58 @@ import (
 
 	rMemo "github.com/maguroguma/go-experimental/internal/repository/memo"
 	sMemo "github.com/maguroguma/go-experimental/internal/service/memo"
+	"go.uber.org/dig"
 )
 
 func main() {
 	fmt.Println("Run...")
+	fmt.Println("")
 
-	r := rMemo.NewRepository()
-	s := sMemo.NewService(r)
+	fmt.Println("simple()")
+	simple()
+	fmt.Println("diByDig()")
+	diByDig()
+
+	fmt.Println("Done.")
+}
+
+func run(s sMemo.Service, l Logger) {
 	m, err := s.GetMemo(1)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("id: %d, text: %s\n", m.ID, m.Text)
+	str := fmt.Sprintf("id: %d, text: %s\n", m.ID, m.Text)
+	l.Log(str)
+}
 
-	fmt.Println("Done.")
+func simple() {
+	r := rMemo.NewRepository()
+	s := sMemo.NewService(r)
+	l := NewLogger()
+
+	run(s, l)
+}
+
+func diByDig() {
+	container := dig.New()
+
+	// Provide する順序は何でも良い
+	if err := container.Provide(NewLogger); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := container.Provide(sMemo.NewService); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := container.Provide(rMemo.NewRepository); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := container.Invoke(run); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/maguroguma/go-experimental
 
 go 1.23.2
 
-require go.uber.org/mock v0.5.0 // indirect
+require (
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/mock v0.5.0 // indirect
+)
